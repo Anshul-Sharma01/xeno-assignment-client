ee8ba50f08bfe18382744639080ad9e19e7c821e
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-09-10 18:06:32 +0530

    completed react typescript setup for frontend

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App(){
+  return(
+    <section className="flex flex-col justify-center items-center h-[100vh]">
+      <h1 className="text-blue-500 text-5xl font-extrabold">Xeno Assignment Frontend - 10/09-25</h1>
+    </section>
+  )
+}
+
+export default App;
